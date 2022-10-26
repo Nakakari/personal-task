bd9e61a45af4fd46492f23fdd3e8aa8bce64aba1
Nakakari <sarinandhika@gmail.com>
2022-10-26 18:50:55 +0700

    first commit

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+import Header from '../header/Header.jsx'
+import Form from '../form/Form.js'
+const Layout = () => {
+    return (
+    <div className="layout">
+      <Header />
+      <Form />
+    </div>
+    )
+}
+
+export default Layout

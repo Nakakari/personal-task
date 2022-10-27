bd1afd5566229b731f566b5d4615bfaeb1919719
Nakakari <sarinandhika@gmail.com>
2022-10-27 21:09:32 +0700

    make it better

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -25,6 +25,7 @@ const Form = ({setTodos, todos}) => {
             setNewContext(newContext= '')
         } else {
             alert('Please complete all fields!')
+            // Haloo
         }
         
        
